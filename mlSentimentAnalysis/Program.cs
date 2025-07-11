8dcc5e678729fcd49d9e0a94a4de2946c2fd4fb9
Shriya <Shriya@LAPTOP-HRTCTSGI>
2025-07-11 13:37:49 +0530

    Add project files.

---
+++ mlSentimentAnalysis/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Load sample data
+using MlSentimentAnalysis;
+
+var sampleData = new SentimentAnalysis.ModelInput()
+{
+    Col0 = @"This is the worst experience I've ever had.",
+};
+
+//Load model and predict output
+var result = SentimentAnalysis.Predict(sampleData);
+Console.WriteLine( result.ToString());
